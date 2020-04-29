ab267a84bc442d66417974e40f93bb9e18f5ea32
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-28 19:16:55 -0700

    Add initial development

---
+++ Develop/routes/api.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+// TODO: import required model/s
+
+// TODO: and add code to the routes so that the app functions correctly
+
+router.post("/api/workouts", (req, res) => {});
+
+router.put("/api/workouts/:id", (req, res) => {});
+
+router.get("/api/workouts", (req, res) => {});
+
+router.get("/api/workouts/range", (req, res) => {});
+
+router.delete("/api/workouts", (req, res) => {});
+
+module.exports = router;
